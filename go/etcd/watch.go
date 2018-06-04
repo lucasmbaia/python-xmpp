7d83dc466fed824aeeaace3ffcf6acca6fd0d61a
lucas.martins <lucas.martins@totvs.com.br>
2018-06-04 15:17:40 -0300

    watch

--- go/etcd/watch.go
+++ go/etcd/watch.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"fmt"
-	"github.com/coreos/etcd/client"
-	"log"
-	"os"
-	"sync"
-	"text/template"
-	"time"
+  "bytes"
+  "strings"
+  "context"
+  "encoding/json"
+  "fmt"
+  "github.com/coreos/etcd/client"
+  "log"
+  "os"
+  "sync"
+  "text/template"
+  "time"
 )
 
 const ha = `
@@ -42,141 +43,159 @@ defaults
 {{printf "\ttimeout check \t\t\t10s"}}
 {{printf "\tmaxconn \t\t\t3000"}}
 {{range .}}
-{{$portDst := .PortDst}}
-listen {{.Application}}
-{{printf "\tbind 192.168.0.1:%s" .PortSrc}}
-{{printf "\tdefault_backend %s" .Application}}
-Application {{.Application}}
+{{printf "listen %s-%s_balance" .Application .PortBind}}
+{{printf "\tbind 192.168.0.1:%s" .PortBind}}
+{{printf "\tuse_backend %s-%s" .Application .PortBind}}
+{{printf "Application %s-%s" .Application .PortBind}}
 {{printf "\tbalance roundrobin"}}
 {{range $index, $ip := .Addresses -}}
-	{{printf "\tserver application-%d %s:%s check" $index $ip $portDst}}
-{{- end}}
+{{printf "\tserver application-%d %s check" $index $ip}}
+{{end -}}
 {{end}}`
 
 type Client struct {
-	client client.KeysAPI
+  client client.KeysAPI
 }
 
-type Infos map[string]InfosLB
+type Infos map[string][]InfosLB
 
 type InfosLB struct {
-	Addresses []string `json:"ips,omitempty"`
-	PortSrc   string   `json:"portSRC,omitempty"`
-	PortDst   string   `json:"portDST,omitempty"`
+  Address string    `json:"address,omitempty"`
+  Ports	  []string  `json:"ports,omitempty"`
 }
 
 type Conf struct {
-	Application string
-	Addresses   []string
-	PortSrc     string
-	PortDst     string
+  Application string
+  Addresses   []string
+  PortBind    string
 }
 
 func New(hosts []string, timeout int) (*Client, error) {
-	var (
-		cli  client.Client
-		err  error
-		resp = &Client{}
-	)
+  var (
+    cli  client.Client
+    err  error
+    resp = &Client{}
+  )
 
-	if cli, err = client.New(client.Config{
-		Endpoints:               hosts,
-		Transport:               client.DefaultTransport,
-		HeaderTimeoutPerRequest: time.Duration(timeout) * time.Second,
-	}); err != nil {
-		return resp, err
-	}
+  if cli, err = client.New(client.Config{
+    Endpoints:               hosts,
+    Transport:               client.DefaultTransport,
+    HeaderTimeoutPerRequest: time.Duration(timeout) * time.Second,
+  }); err != nil {
+    return resp, err
+  }
 
-	resp.client = client.NewKeysAPI(cli)
+  resp.client = client.NewKeysAPI(cli)
 
-	return resp, nil
+  return resp, nil
 }
 
 func (c *Client) Watch(key string, values chan<- string) error {
-	var (
-		watch    client.Watcher
-		err      error
-		response *client.Response
-	)
+  var (
+    watch    client.Watcher
+    err      error
+    response *client.Response
+  )
 
-	for {
-		watch = c.client.Watcher(key, &client.WatcherOptions{Recursive: true})
+  for {
+    watch = c.client.Watcher(key, &client.WatcherOptions{Recursive: true})
 
-		if response, err = watch.Next(context.Background()); err != nil {
-			return err
-		}
+    if response, err = watch.Next(context.Background()); err != nil {
+      return err
+    }
 
-		values <- response.Node.Value
-	}
+    values <- response.Node.Value
+  }
 }
 
 func main() {
-	var (
-		done   = make(chan bool, 1)
-		cli    *Client
-		values = make(chan string)
-		err    error
-		hosts  = []string{"http://192.168.204.128:2379"}
-		infos  Infos
-		mutex  = &sync.Mutex{}
-		t      *template.Template
-		file   *os.File
-	)
+  var (
+    done   = make(chan bool, 1)
+    cli    *Client
+    values = make(chan string)
+    err    error
+    hosts  = []string{"http://172.16.95.183:2379"}
+    infos  Infos
+    mutex  = &sync.Mutex{}
+    t      *template.Template
+    file   *os.File
+  )
 
-	if cli, err = New(hosts, 5); err != nil {
-		log.Fatal(err)
+  if cli, err = New(hosts, 5); err != nil {
+    log.Fatal(err)
+  }
+
+  go func() {
+    for {
+      value := <-values
+
+      mutex.Lock()
+      var conf []Conf
+      var buf bytes.Buffer
+      var address []string
+      var addr string
+      var ports []string
+
+      if file, err = os.Create("haproxy.cfg"); err != nil {
+	log.Println(err)
+	continue
+      }
+
+      if err = json.Unmarshal([]byte(value), &infos); err != nil {
+	log.Println(err)
+	continue
+      }
+
+      for key, info := range infos {
+	ports = []string{}
+
+	for _, port := range info[0].Ports {
+	  ports = append(ports, strings.Split(port, ":")[0])
 	}
 
-	go func() {
-		for {
-			value := <-values
+	for _, p := range ports {
+	  for _, lb := range info {
+	    for _, port := range lb.Ports {
+	      if strings.Split(port, ":")[0] == p {
+		addr = strings.TrimSpace(lb.Address)
+		address = append(address, fmt.Sprintf("%s:%s", strings.Replace(addr, "\"", "", -1), strings.Split(port, ":")[1]))
+	      }
+	    }
+	  }
 
-			mutex.Lock()
-			var conf []Conf
-			var buf bytes.Buffer
+	  conf = append(conf, Conf{Application: key, Addresses: address, PortBind: p})
+	  address = []string{}
+	}
+      }
 
-			if file, err = os.Create("haproxy.cfg"); err != nil {
-				log.Println(err)
-				continue
-			}
+      fmt.Println(conf)
+      t = template.Must(template.New("HA").Parse(ha))
 
-			if err = json.Unmarshal([]byte(value), &infos); err != nil {
-				log.Println(err)
-				continue
-			}
+      if err = t.Execute(&buf, conf); err != nil {
+	log.Println(err)
+	continue
+      }
 
-			for key, info := range infos {
-				conf = append(conf, Conf{Application: key, Addresses: info.Addresses, PortSrc: info.PortSrc, PortDst: info.PortDst})
-			}
+      if _, err = file.Write(buf.Bytes()); err != nil {
+	log.Println(err)
+	continue
+      }
 
-			fmt.Println(conf)
-			t = template.Must(template.New("HA").Parse(ha))
+      if err = file.Close(); err != nil {
+	log.Println(err)
+	continue
+      }
 
-			if err = t.Execute(&buf, conf); err != nil {
-				log.Println(err)
-				continue
-			}
+      fmt.Println(buf.String())
+      mutex.Unlock()
+    }
+  }()
 
-			if _, err = file.Write(buf.Bytes()); err != nil {
-				log.Println(err)
-				continue
-			}
+  go func() {
+    if err = cli.Watch("/haproxy", values); err != nil {
+      log.Fatal(err)
+    }
+  }()
 
-			if err = file.Close(); err != nil {
-				log.Println(err)
-				continue
-			}
-
-			fmt.Println(buf.String())
-			mutex.Unlock()
-		}
-	}()
-
-	go func() {
-		if err = cli.Watch("/python/app", values); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	<-done
+  <-done
 }
