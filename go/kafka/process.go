7e18d1fee33030b958a6858202851e8c16bcd554
lucas.martins <lucas.martins@totvs.com.br>
2018-08-28 09:03:30 -0300

    metrics

--- go/kafka/process.go
+++ go/kafka/process.go
@@ -1,134 +1,204 @@
 package main
 
 import (
-	"os/exec"
-	//"syscall"
-	"fmt"
-	//"os"
-	//"bytes"
-	"encoding/json"
-	"errors"
-	"github.com/Shopify/sarama"
-	"log"
-	"strings"
-	"time"
+  "os/exec"
+  //"syscall"
+  "fmt"
+  "os"
+  //"bytes"
+  "encoding/json"
+  "errors"
+  "github.com/Shopify/sarama"
+  "log"
+  "strings"
+  "time"
 )
 
 type Metrics struct {
-	ContainerID   string `json:",omitempty"`
-	ContainerName string `json:",omitempty"`
-	CpuUsage      string `json:",omitempty"`
-	MemoryUsage   string `json:",omitempty"`
-	MemoryTotal   string `json:",omitempty"`
+  Hostname  string	`json:",omitempty"`
+  Customers []Customers	`json:",omitempty"`
+}
+
+type Customers struct {
+  Name		string		`json:",omitempty"`
+  Applications	[]Applications	`json:",omitempty"`
+}
+
+type Applications struct {
+  Name	      string	    `json:",omitempty"`
+  Containers  []Containers  `json:",omitempty"`
+}
+
+type Containers struct {
+  ContainerID   string `json:",omitempty"`
+  ContainerName string `json:",omitempty"`
+  CpuUsage      string `json:",omitempty"`
+  MemoryUsage   string `json:",omitempty"`
+  MemoryTotal   string `json:",omitempty"`
 }
 
 var r *strings.Replacer = strings.NewReplacer("KiB", "", "MiB", "", "GiB", "")
 
-func dockerStats() ([]Metrics, error) {
-	var (
-		output  []byte
-		err     error
-		metrics []Metrics
-		m       []string
-		info    []string
-	)
+func dockerStats() (Metrics, error) {
+  var (
+    output  []byte
+    err     error
+    metrics Metrics
+    m       []string
+    info    []string
+  )
 
-	if output, err = exec.Command("docker", "stats", "--all", "--no-stream", "--format", "'{{.Container}}#{{ .Name }}#{{.CPUPerc}}#{{.MemUsage}}'").CombinedOutput(); err != nil {
-		return metrics, errors.New(fmt.Sprintf("Code Error: %s, Error Message: %s", err.Error(), string(output)))
+  if output, err = exec.Command("docker", "stats", "--all", "--no-stream", "--format", "'{{.Container}}#{{ .Name }}#{{.CPUPerc}}#{{.MemUsage}}'").CombinedOutput(); err != nil {
+    return metrics, errors.New(fmt.Sprintf("Code Error: %s, Error Message: %s", err.Error(), string(output)))
+  }
+
+  m = strings.Split(strings.Replace(string(output), "'", "", -1), "\n")
+
+  for _, infos := range m {
+    if len(infos) > 0 {
+      info = strings.Split(infos, "#")
+
+      var containsCustomer = false
+      var memory = strings.Split(info[3], "/")
+      var cn = strings.Split(strings.TrimSpace(info[1]), "_app-")
+      var as = strings.Split(cn[1], "-")
+      var app = strings.Join(as[:len(as) -1], "-")
+
+      for i, customer := range metrics.Customers {
+	if customer.Name == cn[0] {
+	  var containsApplication = false
+
+	  for j, application := range metrics.Customers[i].Applications {
+	    if application.Name == app {
+	      metrics.Customers[i].Applications[j].Containers = append(metrics.Customers[i].Applications[j].Containers, Containers{
+		ContainerID:    strings.TrimSpace(info[0]),
+		ContainerName:  strings.TrimSpace(info[1]),
+		CpuUsage:       strings.TrimSpace(strings.Replace(info[2], "%", "", 1)),
+		MemoryUsage:    strings.TrimSpace(memory[0]),
+		MemoryTotal:    strings.TrimSpace(memory[1]),
+	      })
+
+	      containsApplication = true
+	      break
+	    }
+	  }
+
+	  if !containsApplication {
+	    metrics.Customers[i].Applications = append(metrics.Customers[i].Applications, Applications{
+	      Name: app,
+	      Containers: []Containers{{
+		ContainerID:    strings.TrimSpace(info[0]),
+		ContainerName:  strings.TrimSpace(info[1]),
+		CpuUsage:       strings.TrimSpace(strings.Replace(info[2], "%", "", 1)),
+		MemoryUsage:    strings.TrimSpace(memory[0]),
+		MemoryTotal:    strings.TrimSpace(memory[1]),
+	      }},
+	    })
+	  }
+
+	  containsCustomer = true
+	  break
 	}
+      }
 
-	m = strings.Split(strings.Replace(string(output), "'", "", -1), "\n")
+      if !containsCustomer {
+	metrics.Customers = append(metrics.Customers, Customers{
+	  Name:	cn[0],
+	  Applications:	[]Applications{{
+	    Name: app,
+	    Containers:	[]Containers{{
+	      ContainerID:    strings.TrimSpace(info[0]),
+	      ContainerName:  strings.TrimSpace(info[1]),
+	      CpuUsage:	      strings.TrimSpace(strings.Replace(info[2], "%", "", 1)),
+	      MemoryUsage:    strings.TrimSpace(memory[0]),
+	      MemoryTotal:    strings.TrimSpace(memory[1]),
+	    }},
+	  }},
+	})
+      }
+    }
+  }
 
-	for _, infos := range m {
-		if len(infos) > 0 {
-			info = strings.Split(infos, "#")
-
-			var memory = strings.Split(info[3], "/")
-
-			metrics = append(metrics, Metrics{
-				ContainerID:   strings.TrimSpace(info[0]),
-				ContainerName: strings.TrimSpace(info[1]),
-				CpuUsage:      strings.TrimSpace(strings.Replace(info[2], "%", "", 1)),
-				MemoryUsage:   strings.TrimSpace(memory[0]),
-				MemoryTotal:   strings.TrimSpace(memory[1]),
-			})
-		}
-	}
-
-	return metrics, nil
+  return metrics, nil
 }
 
 func main() {
-	var (
-		ticker   *time.Ticker
-		metrics  []Metrics
-		body     []byte
-		err      error
-		config   *sarama.Config
-		producer sarama.SyncProducer
-		msg      *sarama.ProducerMessage
-	)
+  var (
+    ticker    *time.Ticker
+    metrics   Metrics
+    body      []byte
+    err	      error
+    config    *sarama.Config
+    producer  sarama.SyncProducer
+    msg	      *sarama.ProducerMessage
+    hostname  string
+  )
 
-	config = sarama.NewConfig()
-	config.Producer.Retry.Max = 5
+  config = sarama.NewConfig()
+  config.Producer.Retry.Max = 5
 
-	if producer, err = sarama.NewSyncProducer([]string{"192.168.204.134:9092"}, nil); err != nil {
-		log.Fatal(err)
-	}
+  if hostname, err = os.Hostname(); err != nil {
+    log.Fatal(err)
+  }
 
-	defer func() {
-		if err = producer.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+  if producer, err = sarama.NewSyncProducer([]string{"172.16.95.183:9092"}, nil); err != nil {
+    log.Fatal(err)
+  }
 
-	ticker = time.NewTicker(2 * time.Second)
-	defer ticker.Stop()
+  defer func() {
+    if err = producer.Close(); err != nil {
+      log.Fatal(err)
+    }
+  }()
 
-	for {
-		select {
-		case _ = <-ticker.C:
-			if metrics, err = dockerStats(); err != nil {
-				fmt.Println(err)
-				continue
-			}
+  ticker = time.NewTicker(2 * time.Second)
+  defer ticker.Stop()
 
-			if body, err = json.Marshal(metrics); err != nil {
-				fmt.Println(err)
-				continue
-			}
+  for {
+    select {
+    case _ = <-ticker.C:
+      if metrics, err = dockerStats(); err != nil {
+	fmt.Println(err)
+	continue
+      }
 
-			fmt.Println(string(body))
+      metrics.Hostname = hostname
+      if body, err = json.Marshal(metrics); err != nil {
+	fmt.Println(err)
+	continue
+      }
 
-			msg = &sarama.ProducerMessage{
-				Topic: "monitoring",
-				Key:   sarama.ByteEncoder([]byte("meu_ovo")),
-				Value: sarama.ByteEncoder(body),
-			}
+      fmt.Println(string(body))
 
-			if _, _, err = producer.SendMessage(msg); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
+      msg = &sarama.ProducerMessage{
+	Topic: "monitoring",
+	Key:   sarama.ByteEncoder([]byte("meu_ovo")),
+	Value: sarama.ByteEncoder(body),
+      }
 
-	/*var stdout bytes.Buffer
-	  cmd := exec.Command("docker", "stats", "--all", "--no-stream", "--no-trunc", "--format", "'{{.Container}}#{{ .Name }}#{{.CPUPerc}}#{{.MemUsage}}#{{ .NetIO }}'")
-	  cmd.Stdout = &stdout
-	  var waitStatus syscall.WaitStatus
+      if _, _, err = producer.SendMessage(msg); err != nil {
+	fmt.Println(err)
+      }
+    }
+  }
 
-	  if err := cmd.Run(); err != nil {
-	    if err != nil {
-	      os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
-	    }
+  /*var stdout bytes.Buffer
+  cmd := exec.Command("docker", "stats", "--all", "--no-stream", "--no-trunc", "--format", "'{{.Container}}#{{ .Name }}#{{.CPUPerc}}#{{.MemUsage}}#{{ .NetIO }}'")
+  cmd.Stdout = &stdout
+  var waitStatus syscall.WaitStatus
 
-	    if exitError, ok := err.(*exec.ExitError); ok {
-	      waitStatus = exitError.Sys().(syscall.WaitStatus)
-	      fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
-	    }
-	  } else {
-	    fmt.Println(string(stdout.Bytes()))
-	    waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
-	    fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
-	  }*/
+  if err := cmd.Run(); err != nil {
+    if err != nil {
+      os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
+    }
+
+    if exitError, ok := err.(*exec.ExitError); ok {
+      waitStatus = exitError.Sys().(syscall.WaitStatus)
+      fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+    }
+  } else {
+    fmt.Println(string(stdout.Bytes()))
+    waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+    fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+  }*/
 }
